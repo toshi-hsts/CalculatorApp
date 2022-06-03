cbd7c4e5835814611a277e8268c6891da985fd85
toshi <tab.hsts@gmail.com>
2022-06-03 20:28:04 +0900

    layout

--- CalculatorApp/ViewController.swift
+++ CalculatorApp/ViewController.swift
@@ -13,6 +13,13 @@ class ViewController: UIViewController {
     @IBOutlet weak var calculatorCollectionView: UICollectionView!
     @IBOutlet weak var calculatorHeightConstraint: NSLayoutConstraint!
     
+    let numbers = [
+        ["1","2","3","4"],
+        ["1","2","3","4"],
+        ["1","2","3","4"],
+        ["1","2","3","4"],
+        ["1","2","3","4"],
+    ]
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -20,18 +27,21 @@ class ViewController: UIViewController {
         calculatorCollectionView.delegate = self
         calculatorCollectionView.dataSource = self
         calculatorCollectionView.register(UICollectionViewCell.self, forCellWithReuseIdentifier: "cellId")
-        
+        calculatorHeightConstraint.constant = 500
+
     }
-
-
 }
 
 extension ViewController: UICollectionViewDelegate {
 }
 
 extension ViewController: UICollectionViewDataSource {
+    func numberOfSections(in collectionView: UICollectionView) -> Int {
+        return numbers.count
+    }
+    
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        return 20
+        return numbers[section].count
     }
     
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
@@ -40,5 +50,19 @@ extension ViewController: UICollectionViewDataSource {
         cell.backgroundColor = .blue
         return cell
     }
+}
+
+extension ViewController: UICollectionViewDelegateFlowLayout {
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, referenceSizeForHeaderInSection section: Int) -> CGSize {
+        return .init(width: collectionView.frame.width, height: 10)
+    }
     
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        let width = (collectionView.frame.width - 3 * 10) / 4
+        return .init(width: width, height: width)
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumInteritemSpacingForSectionAt section: Int) -> CGFloat {
+        return 10
+    }
 }
